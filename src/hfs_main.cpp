7035e234004618d3e53f9054405d3de59d67ff34
parallels <vnkohm@students.cs.ubc.ca>
2024-04-08 22:22:30 +0200

    Initalized Fuse Arguments and flags properly

--- src/hfs_main.cpp
+++ src/hfs_main.cpp
@@ -4,79 +4,38 @@
 #include <fuse.h>
 #include "hfs.h"
 #include <assert.h>
-
-static const char default_img[] = "myhfs.img";
-
-/* Options passed from commandline arguments */
-struct options {
-    const char *img;
-    int background;
-    int verbose;
-    int show_help;
-    int show_fuse_help;
-} options;
-
+#include <string.h>
 
 struct fuse_operations hfs_oper = {
   .getattr = hfs_getattr
 };
 
-#define OPTION(t, p)                            \
-    { t, offsetof(struct options, p), 1 }
-#define LOPTION(s, l, p)                        \
-    OPTION(s, p),                               \
-    OPTION(l, p)
-
-static const struct fuse_opt option_spec[] = {
-    LOPTION("-i %s",    "--img=%s",     img),
-    LOPTION("-b",       "--background", background),
-    LOPTION("-v",       "--verbose",    verbose),
-    LOPTION("-h",       "--help",       show_help),
-    OPTION(             "--fuse-help",  show_fuse_help),
-    FUSE_OPT_END
-};
-
-static void show_help(const char *progname)
-{
-    printf("usage: %s mountpoint [options]\n\n", progname);
-    printf("By default this FUSE runs in the foreground, and will unmount on\n"
-           "exit. If something goes wrong and FUSE does not exit cleanly, use\n"
-           "the following command to unmount your mountpoint:\n"
-           "  $ fusermount -u <mountpoint>\n\n");
-    printf("common options (use --fuse-help for all options):\n"
-           "    -i, --img=FILE      filename of SFS image to mount\n"
-           "                        (default: \"%s\")\n"
-           "    -b, --background    run fuse in background\n"
-           "    -v, --verbose       print debug information\n"
-           "    -h, --help          show this summarized help\n"
-           "        --fuse-help     show full FUSE help\n"
-           "\n", default_img);
-}
-
-
 int main(int argc, char *argv[]){
 
-    struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
-    fuse_opt_parse(&args, &options, option_spec, NULL);
-    int fuse_stat;
+    std::string mountdir(argv[1]);
+    std::string metadir(argv[2]);
+    std::string datadir(argv[3]);
 
-    if (options.show_help) {
-        show_help(argv[0]);
-        return 0;
-    }
+    std::cout << "Mount directory: " << mountdir << "\n";
+    std::cout << "Metadta directory: " << metadir << "\n";
+    std::cout << "Data directory: " << datadir << "\n";
 
-    if (options.show_fuse_help) {
-        assert(fuse_opt_add_arg(&args, "--help") == 0);
-        args.argv[0][0] = '\0';
-    }
+    char *fuse_argv[20];
+    int fuse_argc = 0;
+    fuse_argv[fuse_argc++] = argv[0];
+    char fuse_mount_dir[100];
+    strcpy(fuse_mount_dir, mountdir.c_str());
+    fuse_argv[fuse_argc++] = fuse_mount_dir;
+    fuse_argv[fuse_argc++] = "-f"; //Run Fuse in the Foreground
+    fuse_argv[fuse_argc++] = "-d"; //Verbose debug output
+    fuse_argv[fuse_argc++] = "-s"; //Single threaded mode
+ 
+    int fuse_state;
 
-    if (!options.background)
-        assert(fuse_opt_add_arg(&args, "-f") == 0);
+    printf("Calling fuse_main...\n");
+    fuse_state = fuse_main(fuse_argc, fuse_argv, &hfs_oper, NULL);
+    printf("fuse_main has exited with code: %d\n", fuse_state);
 
-    fprintf(stderr, "about to call fuse_main\n");
-    fuse_stat = fuse_main(args.argc, args.argv, &hfs_oper, NULL);
-    fprintf(stderr, "fuse_main returned %d\n", fuse_stat);
-    
-    return fuse_stat;
+    return fuse_state;
 }
 
